31e41a9b2b6711de6057e24512683168a53c34f8
suxy <1058952377@qq.com>
2018-07-19 15:12:02 +0800

    改使用外部tomcat部署

--- src/main/java/com/suxy/PicApplication.java
+++ src/main/java/com/suxy/PicApplication.java
@@ -2,9 +2,16 @@ package com.suxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PicApplication {
+public class PicApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PicApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PicApplication.class, args);
